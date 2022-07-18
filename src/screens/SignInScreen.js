87238b002d9ae0772649964d94f7b3b3c9d69648
aureliane dor <aureliane.dor@gmail.com>
2022-07-18 17:18:31 +0200

    error firebase

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import {
-  StyleSheet,
-  View,
-  Text,
-  Alert,
-  SafeAreaView,
-  Image,
-  KeyboardAvoidingView,
+    StyleSheet,
+    View,
+    Text,
+    Alert,
+    SafeAreaView,
+    Image,
+    KeyboardAvoidingView,
 } from 'react-native';
 import Button from '../components/Button';
 import Strings from '../const/Strings';
@@ -19,74 +19,116 @@ import DismissKeyboard from '../components/DismissKeyboard';
 import EmailTextField from '../components/EmailTextField';
 import PasswordTextField from '../components/PasswordTextField';
 
-
+import firebase from '../firebase/Firebase';
 
 function SignInScreen() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [isLoading, setIsLoading] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [isLoading, setIsLoading] = useState('');
 
-  function validateEmailAddress() {
-    const isValidEmail = Utility.isEmailValid(email);
-    isValidEmail
-      ? setEmailError('')
-      : setEmailError(Strings.InvalidEmailAddress);
-    return isValidEmail;
-  }
+    function validateEmailAddress() {
+        const isValidEmail = Utility.isEmailValid(email);
+        isValidEmail
+            ? setEmailError('')
+            : setEmailError(Strings.InvalidEmailAddress);
+        return isValidEmail;
+    }
 
-  function validatePasswordField() {
-    const isValidField = Utility.isValidField(password);
-    isValidField
-      ? setPasswordError('')
-      : setPasswordError(Strings.PasswordFieldEmpty);
-    return isValidField;
-  }
+    function validatePasswordField() {
+        const isValidField = Utility.isValidField(password);
+        isValidField
+            ? setPasswordError('')
+            : setPasswordError(Strings.PasswordFieldEmpty);
+        return isValidField;
+    }
 
-  return (
-    <DismissKeyboard>
-      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-        <View>
-          <SafeAreaView>
-            <Image style={styles.logo} source={Images.logo}></Image>
-            <EmailTextField
-              term={email}
-              error={emailError}
-              placeHolder={Strings.EmailPlaceHolder}
-              onTermChange={newEmail => {
-                setEmail(newEmail);
-              }}
-              onValidateEmailAddress={validateEmailAddress}
-            />
-            <PasswordTextField
-              term={password}
-              error={passwordError}
-              placeholder={Strings.PasswordPlaceHolder}
-              onTermChange={newPassword => {
-                setPassword(newPassword)
-              }}
-              onValidatePasswordField={validatePasswordField}
-            />
-            <Button title={Strings.Join} isLoading={isLoading} />
-          </SafeAreaView>
-        </View>
-      </KeyboardAvoidingView>
-    </DismissKeyboard>
-  );
+    const performAuth = () => {
+        const isValidEmail = validateEmailAddress();
+        const isValidPassword = validatePasswordField();
+
+        if (isValidEmail && isValidPassword) {
+            setEmailError('');
+            setPasswordError('');
+            registration(email, password);
+        }
+    };
+
+    const registration = (email, password) => {
+        try {
+            setIsLoading(true);
+
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(email, password)
+                .then(user => {
+                    setIsLoading(false);
+                    Alert.alert('Logged In');
+                })
+                .catch(error => {
+                    firebase
+                        .auth()
+                        .createUserWithEmailAndPassword(email, password)
+                        .then(user => {
+                            setIsLoading(false);
+                            Alert.alert('Create a new user');
+                        })
+                        .catch(error => {
+                            setIsLoading(false);
+                            console.log('error');
+                            Alert.alert(error);
+                        });
+                });
+        } catch (error) {
+            setIsLoading(false);
+            Alert.alert(error);
+        }
+    };
+
+    return (
+        <DismissKeyboard>
+            <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+                <View>
+                    <SafeAreaView>
+                        <Image style={styles.logo} source={Images.logo}></Image>
+                        <EmailTextField
+                            term={email}
+                            error={emailError}
+                            placeHolder={Strings.EmailPlaceHolder}
+                            onTermChange={newEmail => {
+                                setEmail(newEmail);
+                            }}
+                            onValidateEmailAddress={validateEmailAddress}
+                        />
+                        <PasswordTextField
+                            term={password}
+                            error={passwordError}
+                            placeholder={Strings.PasswordPlaceHolder}
+                            onTermChange={newPassword => {
+                                setPassword(newPassword);
+                            }}
+                            onValidatePasswordField={validatePasswordField}
+                        />
+                        <Button title={Strings.Join} isLoading={isLoading}/>
+                    </SafeAreaView>
+                </View>
+            </KeyboardAvoidingView>
+        </DismissKeyboard>
+    );
 }
 
 const styles = StyleSheet.create({
-  logo: {
-    alignSelf: 'center',
-    margin: 0.04 * Constants.screenHeight,
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Color.theme,
-  },
+    logo: {
+        alignSelf: 'center',
+        margin: 0.04 * Constants.screenHeight,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Color.theme,
+    },
 });
 
 export default SignInScreen;
