bb8a927f3acf37513e9f871aed5db13c3b6e01aa
aureliane dor <aureliane.dor@gmail.com>
2022-07-19 10:18:17 +0200

    correct id

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,16 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import {project} from 'react-native/react-native.config';
 
 require('firebase/auth');
 
 const prodCredentials = {
-    apiKey: 'AIzaSyDsQyNOSloA_i9L9rDUjtqlpmYbsMCtwmk',
-    authDomain: 'chivteisrael-25708.firebaseapp.com',
-    projectId: 'chivteisrael-25708',
-    storageBucket: 'chivteisrael-25708.appspot.com',
-    messagingSenderId: '371600271082',
-    appId: '1:371600271082:web:50a2cfa3172c3425e01993',
-    measurementId: 'G-SJXCHBMRN5',
+    apiKey: 'AIzaSyDvx3hFbEk0eaSo-shl2u0Ly5IK-uLWe9k',
+    authDomain: 'udemychatapp-63fba.firebaseapp.com',
+    projectId: 'udemychatapp-63fba',
+    storageBucket: 'udemychatapp-63fba.appspot.com',
+    messagingSenderId: '805531774827',
+    appId: '1:805531774827:web:b85311524b6b73a986e62e',
 };
 
 firebase.initializeApp(prodCredentials);
