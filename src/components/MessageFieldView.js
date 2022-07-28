a1d9896b5808dfc01cf49c027d4598c03057398d
aureliane dor <aureliane.dor@gmail.com>
2022-07-28 12:18:07 +0200

    Message Field View

---
+++ src/components/MessageFieldView.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {TextInput, Text, StyleSheet, View, Button} from 'react-native';
+import Color from '../utils/Color';
+import Constants from '../const/Constants';
+import Button from '../components/Button';
+import Strings from '../const/Strings';
+
+const MessageFieldView = ({
+  term,
+  placeholder,
+  onTermChange,
+  onValidateTextField,
+  error,
+  onSubmit,
+  isJoined,
+}) => {
+  return (
+    <View style={styles.containerView}>
+      <View style={styles.fieldView}>
+        <TextInput
+          autoCorrect={false}
+          style={styles.textField}
+          placeholder={placeholder}
+          value={term}
+          onChangeText={onTermChange}
+          onEndEditing={onValidateTextField}
+        />
+          <Button title={Strings.Send} color={Color.white} onPress={onSubmit} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerView: {
+    backgroundColor: Color.white,
+    width: Constants.screenWidth,
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+
+  fieldView: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: Color.uaStudiosGreen,
+  },
+
+  textField: {
+    fontSize: 14,
+    flex: 1,
+    marginRight: 10,
+    paddingLeft: 10,
+    width: '75%',
+    borderColor: Color.gray,
+    borderWidth: 1,
+    justifyContent: 'center',
+    backgroundColor: Color.smoke,
+  },
+
+  button: {
+    flex: 1,
+    alignSelf: 'center',
+    width: '25%',
+    height: '100%',
+  },
+});
+
+export default MessageFieldView;
--- src/const/Strings.js
+++ src/const/Strings.js
@@ -1,12 +1,13 @@
 const Strings = {
-    Join: 'Join Now',
-    InvalidEmailAddress: 'Invalid Email Address !',
-    PasswordFieldEmpty: 'Password Field cannot be empty !',
-    EmailPlaceHolder: 'Email Address',
-    PasswordPlaceHolder: 'Password',
-    EnterYourGroupName: 'Enter your group name',
-    GroupNameEmpty: 'Group name cannot be empty',
-    CreateGroup: 'Create Group',
+  Join: 'Join Now',
+  InvalidEmailAddress: 'Invalid Email Address !',
+  PasswordFieldEmpty: 'Password Field cannot be empty !',
+  EmailPlaceHolder: 'Email Address',
+  PasswordPlaceHolder: 'Password',
+  EnterYourGroupName: 'Enter your group name',
+  GroupNameEmpty: 'Group name cannot be empty',
+  CreateGroup: 'Create Group',
+  Send: 'Send',
 };
 
 export default Strings;
